880a68692925b74a740fc452dd6430309c662a78
VhorsSeth <62798650+VhorsSeth@users.noreply.github.com>
2020-03-28 20:14:11 +0700

    Add files via upload

---
+++ gtmb by busy/growtopia/server_data.php
@@ -0,0 +1,11 @@
+server|34.66.50.128
+port|17091
+type|1
+#maint|Mainetrance message (Not used for now) -- Growtopia Noobs
+
+beta_server|127.0.0.1
+beta_port|17091
+
+beta_type|1
+meta|localhost
+RTENDMARKERBS1001
